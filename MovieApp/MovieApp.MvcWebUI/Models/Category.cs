55d4e545490840c6aec83f6197aba509c56b73fe
FatmaKaya <kaya.fatma.kaya@hotmail.com>
2019-07-18 23:08:43 +0300

    Added Menu Components and filter movies by category.

--- MovieApp/MovieApp.MvcWebUI/Models/Category.cs
+++ MovieApp/MovieApp.MvcWebUI/Models/Category.cs
@@ -5,14 +5,10 @@ using System.Threading.Tasks;
 
 namespace MovieApp.MvcWebUI.Models
 {
-    public class Movie
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
-
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public List<Movie> Movies { get; set; }
     }
 }
